29b0949108b922d126c8e1c071dbe68683c78ebd
EmpAsh938 <ashokthapa938@gmail.com>
2024-10-14 21:38:41 +0545

    sharp added for server image

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "elearningplatform-production.up.railway.app",
-                pathname: "/api/v1/categories/image/**",
-            },
-            {
-                protocol: "https",
-                hostname: "elearningplatform-production.up.railway.app",
-                pathname: "/api/v1/users/image/**",
-            },
-        ],
+        domains: ['elearningplatform-production.up.railway.app'],
     },
 };
 
